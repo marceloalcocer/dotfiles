673ea7c2e86abf097725a1f7bba1b338aa092ad1
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-07-05 23:41:42 +0200

    Add mouse wheel

--- qmk/keymap/keymap.c
+++ qmk/keymap/keymap.c
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ╭────────┬──────┬──────┬──────┬──────┬──────╮                              ╭──────┬──────┬──────┬──────┬──────┬────────╮
  * │        │   1  │  2   │  3   │  4   │  5   │                              │  6   │  7   │  8   │  9   │  0   │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┤                              ├──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │      │ Prev │ Play │ Next │ Mute │                              │ Left │ Down │ Up   │ Right│      │        │
+ * │ Prev   │ Play │ Next │ Mute │VolDwn│VolUp │                              │ Left │ Down │ Up   │ Right│      │        │
  * ├────────┼──────┼──────┼──────┼──────┼──────┼──────┬──────╮  ╭──────┬──────┼──────┼──────┼──────┼──────┼──────┼────────┤
- * │        │      │      │      │      │      │      │      │  │      │      │ MLeft│ Mdown│ MUp  │MRight│      │        │
+ * │        │      │      │MicMut│BriDwn│BriUp │      │      │  │      │      │ MLeft│ Mdown│ MUp  │MRight│      │        │
  * ╰────────┴──────┴──────┼──────┼──────┼──────┤      │      │  │      │      ├──────┼──────┼──────┼──────┴──────┴────────╯
  *                        │      │      │      │      │      │  │      │      │      │      │      │
  *                        │Shtdwn│      │      │      │      │  │      │      │ MBtn1│ MBtn2│Logout│
@@ -81,8 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [RAISE] = LAYOUT_split_3x6_5(
       _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-      _______, _______, _______, _______, _______, _______,                                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+      KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,                                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+      _______, _______, _______, KC_F20,  KC_BRID, KC_BRIU,                                     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
                                  LCA(KC_END), _______, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, LCA(KC_DEL)
     ),
 /*
@@ -204,9 +204,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         // Volume control
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code(KC_WH_D);
         } else {
-            tap_code(KC_VOLD);
+            tap_code(KC_WH_U);
         }
     }
     else if (index == 1) {
