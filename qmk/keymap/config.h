bc9867525d92e628d0e097b8d7dc524dd21a0931
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-08-09 21:45:45 +0200

    Adjust tapping term

--- qmk/keymap/config.h
+++ qmk/keymap/config.h
@@ -29,6 +29,9 @@
 #define ENCODER_DIRECTION_FLIP
 #define ENCODER_DIRECTION_FLIP_RIGHT
 
+// Tweak tapping term
+#define TAPPING_TERM 150
+
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 //#define SPLIT_USB_DETECT
 //#define NO_USB_STARTUP_CHECK
